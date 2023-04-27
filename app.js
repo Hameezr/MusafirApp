1680a3024106ca4bc2f5cd1b7fedfe29646c5536
Hameez Rizwan <hameexr@gmail.com>
2023-04-28 01:58:35 +0500

    error handling middleware added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -17,5 +18,35 @@ app.use((req, res, next)=>{
 // Below process is called mounting the router on a route 
 app.use('/api/v1/tours', tourRouter)
 app.use('/api/v1/users', userRouter)
+
+// handling for wrong routes below, doing it after the correct routes above as it will only execute if above routes are not 
+// matched
+
+app.all('*', (req, res, next)=> { // app.all is used for all app.get/post/delete etc and '*' is used for all routes
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can\'t find the URL ${req.originalUrl} on this server`
+  // })
+
+  // Below is the implementation if I do not have a class for AppError
+  // const err = new Error(`Can\'t find the URL ${req.originalUrl} on this server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  const err = new AppError(`Can\'t find the URL ${req.originalUrl} on this server`, 404);
+  next(err); // if next receives an argument, express will assume there's an error, next will pass error to the next middleware
+})
+
+// ERROR HANDLING MIDDLEWARE 
+app.use((err, req, res, next)=> { // 4 parameters and express will automatically understand its an error handling middleware
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  })
+  res.status();
+})
 
 module.exports = app;

2e44b6ac7f34fe115b576710828185abcd824ee3
Hameez Rizwan <hameexr@gmail.com>
2023-05-11 20:04:26 +0500

    app Error implemented

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -38,16 +39,7 @@ app.all('*', (req, res, next)=> { // app.all is used for all app.get/post/delete
 })
 
 // ERROR HANDLING MIDDLEWARE 
-app.use((err, req, res, next)=> { // 4 parameters and express will automatically understand its an error handling middleware
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  })
-  res.status();
-})
+app.use(globalErrorHandler)
 
 
 module.exports = app;

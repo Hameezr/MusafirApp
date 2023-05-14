71be0b5f21742d614cb15734066bd2448b3f4ecb
Hameez Rizwan <hameexr@gmail.com>
2023-05-14 14:06:26 +0500

    implemented basic signup

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const userController = require('../controllers/userControllers');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
 
 const {getAllUsers, createUser, getUser, updateUser, deleteUser} = userController; 
+
+router.post('/signup', authController.signup)
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
